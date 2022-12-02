6516219d815002190256fce0108c7a6b2a2e0eb0
Rahul Patil <rahulp@cdac.in>
2022-12-02 08:33:01 +0530

    initial commit

---
+++ src/main/java/com/rspatil45/pc/PetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.rspatil45.pc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rspatil45/pc/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rspatil45.pc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
